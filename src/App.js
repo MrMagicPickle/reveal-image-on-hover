d1763d812130cae383f6312252dca2218861d79f
Wei Shen Tan <tanweishen97@gmail.com>
2023-04-09 22:58:01 +0800

    Clean up commented code and finalized values

--- src/App.js
+++ src/App.js
@@ -41,43 +41,9 @@ function ImagePlane() {
     uniform float uTime;
     uniform vec2 uMouseUv;
 
-    vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
     vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
     vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
     vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
-
-    float cnoise(vec2 P){
-      vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
-      vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
-      Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
-      vec4 ix = Pi.xzxz;
-      vec4 iy = Pi.yyww;
-      vec4 fx = Pf.xzxz;
-      vec4 fy = Pf.yyww;
-      vec4 i = permute(permute(ix) + iy);
-      vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
-      vec4 gy = abs(gx) - 0.5;
-      vec4 tx = floor(gx + 0.5);
-      gx = gx - tx;
-      vec2 g00 = vec2(gx.x,gy.x);
-      vec2 g10 = vec2(gx.y,gy.y);
-      vec2 g01 = vec2(gx.z,gy.z);
-      vec2 g11 = vec2(gx.w,gy.w);
-      vec4 norm = 1.79284291400159 - 0.85373472095314 *
-        vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
-      g00 *= norm.x;
-      g01 *= norm.y;
-      g10 *= norm.z;
-      g11 *= norm.w;
-      float n00 = dot(g00, vec2(fx.x, fy.x));
-      float n10 = dot(g10, vec2(fx.y, fy.y));
-      float n01 = dot(g01, vec2(fx.z, fy.z));
-      float n11 = dot(g11, vec2(fx.w, fy.w));
-      vec2 fade_xy = fade(Pf.xy);
-      vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
-      float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
-      return 2.3 * n_xy;
-    }
 
     float cnoise(vec3 P){
       vec3 Pi0 = floor(P); // Integer part for indexing
@@ -147,48 +113,24 @@ function ImagePlane() {
       return 2.2 * n_xyz;
     }
 
-    float fnoise(float x) {
-      return fract(sin(x) * 43758.5453123);
-    }
-
     void main() {
       vec3 color = vec3(vUv, 1.);
       vec4 texture = texture2D(uTexture, vUv);
-      vec3 mixed = mix(texture.rgb, color, vUv.x);
-
 
       float noise = cnoise(vec3(vUv, uTime * 0.2));
       noise = smoothstep(0.0, 0.001, noise);
 
-      mixed = mix(texture.rgb, color, 1. - noise);
-
-      float opacity = 1.;
-      float opacityNoise = cnoise(vec3(vUv, uTime * 0.1));
-      opacityNoise = smoothstep(0., 0.001, opacityNoise);
-
-
-      // float edgeDist = length(uMouseUv - vec2(0.5)) * 2.0;
-      // float noiseValue = fnoise(edgeDist * 10.0) * 0.1;
-      // opacity = smoothstep(0.5 - noiseValue, 0.5 + noiseValue, edgeDist);
-
-
-      float edgeDist = distance((vUv), uMouseUv) * 0.9;
+      float edgeDist = distance((vUv), uMouseUv) * 0.8;
       edgeDist = smoothstep(0.0, 0.7, edgeDist);
-      float xNoise = max(0., cnoise(vec3(edgeDist, edgeDist, uTime)));
-      xNoise = max(0.2, cnoise(vec3(edgeDist * 10., 10. * ((vUv.y + vUv.x ) / 2.), uTime * 0.5)));
-      // xNoise = max(0.2, cnoise(vec3(vUv, edgeDist * 2.)));
-      // xNoise = max(0., cnoise(vec3(edgeDist, vUv.x, vUv.y* 5.)));
+
+      // xNoise refers to the noise around the circle distance function
+      float xNoise = max(0.2, cnoise(vec3(edgeDist * 10., 10. * ((vUv.y + vUv.x ) / 2.), uTime * 0.6)));
       edgeDist = edgeDist - xNoise;
       edgeDist = smoothstep(0.0, 0.2, edgeDist);
       edgeDist = floor(edgeDist);
-      // gl_FragColor = vec4(color, 1.);
-      // gl_FragColor = texture;
-      // gl_FragColor = vec4(vec3(noise), 1.);
 
-      mixed = mix(texture.rgb, color, edgeDist);
-      gl_FragColor = vec4(mixed, opacity);
-      // gl_FragColor = vec4(vec3(edgeDist), 1.);
-      // gl_FragColor = vec4(vec3(xNoise), 1.);
+      vec3 mixed = mix(texture.rgb, color, edgeDist);
+      gl_FragColor = vec4(mixed, 1.);
     }
   `;
 
@@ -210,7 +152,6 @@ function ImagePlane() {
         fragmentShader={fragmentShader}
         uniforms={uniforms}
         transparent
-        // wireframe
       />
     </mesh>
   </>
